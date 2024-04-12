9f6e7b7e9279bcb9472b7a401bb3017c0dee48ed
Микита Шевцов <ipz222_shms@student.ztu.edu.ua>
2024-04-12 15:27:48 +0300

    Prepare console app for tests

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleApp;
 
-Console.WriteLine("Hello, World!");
+while (true)
+{
+    var i = 0;
+    Console.WriteLine("Select scenario:");
+    foreach (var scenario in Enum.GetValues<Scenario>())
+        Console.WriteLine($"\t{++i}: {scenario}");
+
+    Console.Write("\nEnter number: ");
+    var input = Console.ReadLine();
+    int selectedScenario;
+    if (int.TryParse(input, out var iValue))
+        selectedScenario = --iValue;
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Invalid input!\n");
+        continue;
+    }
+
+    Console.Clear();
+    if (!Enum.IsDefined(typeof(Scenario), selectedScenario))
+    {
+        Console.WriteLine("Unknown scenario!\n");
+        continue;
+    }
+    
+    Console.WriteLine($"Run '{(Scenario)selectedScenario}' scenario:\n\n");
+
+    switch ((Scenario)selectedScenario)
+    {
+        case Scenario.Factory:
+            break;
+        case Scenario.AbstractFactory:
+            break;
+        case Scenario.Singleton:
+            break;
+        case Scenario.Prototype:
+            break;
+        case Scenario.Builder:
+            break;
+        default:
+            throw new NotImplementedException();
+    }
+    
+    Console.Write("\n\nPress any key to continue...");
+    Console.ReadKey();
+    Console.Clear();
+}
